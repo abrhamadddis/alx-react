dcc94d337a0e2aced4b83afa5aa9d14cdf03a653
abrhamadddis <abrhamaddis32@gmail.com>
2023-01-30 23:30:46 +0300

    Basic application

---
+++ 0x01-react_intro/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={logo}  alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <hr className="line-break"></hr>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </div>
+      <hr className="line-break"></hr>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
