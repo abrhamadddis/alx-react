b64f04e83fbbe2f96fe0377c6452debd8273bc55
abrhamadddis <abrhamaddis32@gmail.com>
2023-02-01 23:09:36 +0300

    modify the app

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
@@ -13,10 +14,17 @@ function App() {
         <p>
           Login to access the full dashboard
         </p>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email"></input>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password"></input>
+          <button>OK</button>
+        </form>
       </div>
       <hr className="line-break"></hr>
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
       </div>
 
     </div>
