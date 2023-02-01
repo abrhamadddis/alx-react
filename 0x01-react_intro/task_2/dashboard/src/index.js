42cc9593bf08a01cad2f5e41f14237a43e9da4f5
abrhamadddis <abrhamaddis32@gmail.com>
2023-02-01 23:49:26 +0300

    module error

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootNotifications = ReactDom.createRoot(document.getElementById('root-notifications'));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+)
 root.render(
   <React.StrictMode>
     <App />
